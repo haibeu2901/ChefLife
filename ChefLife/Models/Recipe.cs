e0ac67070b4d586f4419cf424c1d0df0dfe3ada7
haibeu2901 <beu2901@gmail.com>
2025-05-06 16:11:52 +0700

    create class Recipe

---
+++ ChefLife/Models/Recipe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChefLife.Models
+{
+    internal class Recipe
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Dictionary<string, int> Ingredients { get; set; }
+        public int Difficulty { get; set; }
+        public decimal BasePrice { get; set; }
+
+        public Recipe(string name, string description, Dictionary<string, int> ingredients, int difficulty, decimal basePrice)
+        {
+            Name = name;
+            Description = description;
+            Ingredients = ingredients;
+            Difficulty = Math.Clamp(difficulty,1,5);
+            BasePrice = basePrice;
+        }
+
+        public string GetDifficultyStars()
+        {   
+            return new string('★', Difficulty) + new string('☆', 5 - Difficulty);
+        }
+
+        public void DisplayRecipe()
+        {
+            Console.WriteLine($"===== {Name} =====");
+            Console.WriteLine(Description);
+            Console.WriteLine($"Difficulty: {GetDifficultyStars()}");
+            Console.WriteLine($"Base Price: ${BasePrice}");
+            Console.WriteLine("Ingredients:");
+            foreach (var ingre in Ingredients)
+            {
+                Console.WriteLine($"{ingre.Key}: {ingre.Value}");
+            }
+            Console.WriteLine("================");
+        }
+    }
+}
