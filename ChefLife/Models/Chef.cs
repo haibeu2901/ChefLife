4c07dfe9e9cbdfd286f04aa30653d801367ce743
haibeu2901 <beu2901@gmail.com>
2025-05-06 15:55:22 +0700

    create chef class

---
+++ ChefLife/Models/Chef.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChefLife.Models
+{
+    internal class Chef
+    {
+        public string Name {  get; set; }
+        public int CoolingLevel { get; set; }
+        public decimal Money { get; set; }
+        public int Reputation {  get; set; }
+        public Dictionary<string, int> Inventory { get; set; }
+        
+    }
+}
