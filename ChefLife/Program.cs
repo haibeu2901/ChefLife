c6e340b0065d2793403865e41ddf5dc230b0c8bd
haibeu2901 <beu2901@gmail.com>
2025-05-08 11:35:12 +0700

    debug on recipes difficulty star

--- ChefLife/Program.cs
+++ ChefLife/Program.cs
@@ -6,6 +6,9 @@ namespace ChefLife
     {
         static void Main(string[] args)
         {
+            // Set the Console Output Encoding to UTF-8
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             Console.WriteLine("Welcome to Chef's Life!");
             Console.WriteLine("Loading game...");
 
