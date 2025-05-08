fb6ea0509801b12c1275d9294c2e99f0443acf0c
haibeu2901 <beu2901@gmail.com>
2025-05-08 11:25:29 +0700

    final finish of the game

--- ChefLife/Program.cs
+++ ChefLife/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ChefLife
+﻿using ChefLife.Core;
+
+namespace ChefLife
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Chef's Life!");
+            Console.WriteLine("Loading game...");
+
+            // Create and start the game
+            Game game = new Game();
+            game.Start();
+
+            Console.WriteLine("Thanks for playing Chef's Life!");
         }
     }
 }
